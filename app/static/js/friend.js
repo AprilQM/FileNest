dd09e1e5481a5f018538b0ad1fa4100d732f95b4
unknown <administrator@HADOOP.NET>
2024-12-13 13:18:03 +0800

    0.4.9

--- app/static/js/friend.js
+++ app/static/js/friend.js
@@ -2,10 +2,11 @@ highlight_menu_item(6)
 
 function get_chat_histroy(element, target_user_name) {
     element.classList.remove("friend_message_box")
-    
+    $id("content_box").classList.add("right")
     const param = {
         "target_user_name": target_user_name
     }
+
 
     ajax('POST', "/api/get_chat_histroy",param, function (response) {
         
