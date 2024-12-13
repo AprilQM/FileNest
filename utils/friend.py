1c1ca65984518fdcffce25e4c4fb366f790e315e
unknown <administrator@HADOOP.NET>
2024-12-13 12:57:38 +0800

    0.4.8

--- utils/friend.py
+++ utils/friend.py
@@ -22,10 +22,18 @@ def add_friend(target_user_id):
             user_folder = os.path.join(Config.USER_INFO_DIR, str(current_user.user_id))
             user_file_path = os.path.join(user_folder, "user_info.json")
             
+
+            del user_data["user_datas"]["password"]
+            del user_data["user_datas"]["next_level_need_days"]
+            del user_data["user_space_info"]["praise_count"]
+            del user_data["other"]
+
             with open(user_file_path, "w", encoding="utf-8") as file:
                     json.dump(user_data, file, ensure_ascii=False, indent=4)
             
             os.mkdir(os.path.join(Config.USER_INFO_DIR, str(current_user.user_id), "chat", str(target_user_id)))
+            with open(os.path.join(Config.USER_INFO_DIR, str(current_user.user_id), "chat", str(target_user_id), "1"), "w+", encoding="utf-8") as f:
+                f.write("")
         
         # 对象创建好友
         user_datas = database.get_user(target_user_id)
@@ -40,10 +48,18 @@ def add_friend(target_user_id):
             user_folder = os.path.join(Config.USER_INFO_DIR, str(target_user_id))
             user_file_path = os.path.join(user_folder, "user_info.json")
 
+            del user_data["user_datas"]["password"]
+            del user_data["user_datas"]["next_level_need_days"]
+            del user_data["user_space_info"]["praise_count"]
+            del user_data["other"]
+
+
             with open(user_file_path, "w", encoding="utf-8") as file:
                 json.dump(user_data, file, ensure_ascii=False, indent=4)
 
             os.mkdir(os.path.join(Config.USER_INFO_DIR, str(target_user_id), "chat", str(current_user.user_id)))
+            with open(os.path.join(Config.USER_INFO_DIR, str(target_user_id), "chat", str(current_user.user_id), "1"), "w+", encoding="utf-8") as f:
+                f.write("")
 
         return {"success": True, "message": "success"}
         
